fefe3d3d11c516d5c45b71f507b1eb5f51471c40
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:17:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-queen-dev-133417",
+  url: "https://muddy-queen-dev-133417.botics.co",
+  api: "https://muddy-queen-dev-133417.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
